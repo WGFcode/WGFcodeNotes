86e8b04e3b60b046e55bddf2f2cb587fd12a12cc
baiaicaiai <wugong@buybal.com>
2020-03-31 16:30:18 +0800

    add photo

--- WGFcodeNotes/WGMainVC.swift
+++ WGFcodeNotes/WGMainVC.swift
@@ -13,5 +13,44 @@ public class WGMainVC : UIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.green
+        
+        let thread1 = Thread(target: self, selector: #selector(testMethod), object: nil)
+        //开启线程
+        thread1.start()
+        /*属性含义
+         name: 设置线程名称
+         threadDictionary: 每个线程都有个字典，在线程中任何地方被访问
+         threadPriority:设置线程优先级(0-1.0)
+         stackSize: 线程使用栈区大小(默认是512K)
+         isExecuting: 线程是否正在执行
+         isFinished: 线程是否执行完成
+         isCancelled: 线程是否撤销
+         isMainThread: 是否是主线程
+         */
+        //2.类方法创建线程并自动开启
+        Thread.detachNewThreadSelector(#selector(textMethod1(title:)), toTarget: self, with: "传递给调用方法的参数")
+        if #available(iOS 10.0, *) {
+            Thread.detachNewThread {
+                NSLog("Block方式创建线程并启动")
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+        /*
+         调用类方法阻塞当前线程
+         Thread.sleep(until: <#T##Date#>)
+         Thread.sleep(forTimeInterval: <#T##TimeInterval#>)
+         //推出当前线程
+         Thread.exit()
+         */
+        
+        
+    }
+    
+    @objc func testMethod() {
+    }
+    
+    @objc func textMethod1(title: String) {
+        NSLog("\(title)")
     }
 }
