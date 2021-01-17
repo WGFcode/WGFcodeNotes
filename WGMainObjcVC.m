20fae0cf454df785d457ab9fed7080f03ab5e009
baiaicaiai <1813061716@qq.com>
2021-01-17 23:04:13 +0800

    property

--- WGMainObjcVC.m
+++ WGMainObjcVC.m
@@ -34,6 +34,18 @@
     person2.name= @"li si";
     NSLog(@"name is %@",person2.name);
     
+    //文字计算
+    [@"sdf" boundingRectWithSize:CGSizeMake(100, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:nil context:nil];
+    //文字绘制
+    [@"test" drawWithRect:CGRectMake(0, 0, 100, 100) options:NSStringDrawingUsesLineFragmentOrigin attributes:nil context:nil];
+    
+    UIImageView *imgView = [[UIImageView alloc]init];
+    //通过这种方式加载图片,其实是不会直接显示到屏幕上的,加载的其实是经过压缩后的二进制数据
+    //如果要渲染到屏幕上,还需再经过解码,解码成屏幕需要的格式,而解码是放在主线程的,所以可能会产生卡顿
+    //可以把解码放在子线程,具体如何解码可以参考网上好多第三方的库中找到
+    imgView.image = [UIImage imageNamed:@"test"];
+    [self.view addSubview:imgView];
+    
 }
 @end
 
