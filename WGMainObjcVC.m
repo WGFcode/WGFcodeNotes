05f66eb621f117133ff4a6476dede92b3bc209d4
Fcode <wugong@chinanums.com>
2021-01-28 18:19:34 +0800

    fix bug

--- WGMainObjcVC.m
+++ WGMainObjcVC.m
@@ -18,40 +18,27 @@
 #import <pthread.h>
 
 @interface WGMainObjcVC()
-@property(nonatomic, strong) dispatch_queue_t queue;
 @end
 
 
 @implementation WGMainObjcVC
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
 
-    //1. 手动创建的并发队列
-    self.queue = dispatch_queue_create("myqueue", DISPATCH_QUEUE_CONCURRENT);
 
-    for (int i = 0; i < 10; i++) {
-        [self read];
-        [self write];
-    }
 }
 
-//从文件中读取内容
--(void)read {
-    //2. 读时:
-    dispatch_async(self.queue, ^{
-        sleep(1);
-        NSLog(@"read");
-    });
-}
 
-//往文件中写入内容
--(void)write {
-    //3. 写时: 调用dispatch_barrier_async函数
-    dispatch_barrier_async(self.queue, ^{
-        sleep(1);
-        NSLog(@"write");
-    });
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSThread *thread = [[NSThread alloc]initWithBlock:^{
+        NSLog(@"执行任务1");
+    }];
+    [thread start];
+    [self performSelector:@selector(test) onThread:thread withObject:nil waitUntilDone:YES];
+}
+-(void)test{
+    NSLog(@"执行任务2");
 }
 
 @end
