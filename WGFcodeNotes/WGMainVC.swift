f83b834338f964f370234c73806b0964d6187ec1
Fcode <wugong@chinanums.com>
2021-04-21 17:17:31 +0800

    struct

--- WGFcodeNotes/WGMainVC.swift
+++ WGFcodeNotes/WGMainVC.swift
@@ -10,12 +10,21 @@ import Foundation
 import UIKit
 
 
-struct DirectionStruct {
-    var height: Float = 1
-    var name = "a"
-    var sex = true  
-    //实际占用内存大小:25 系统分配内存大小:32 内存对齐大小:8
+enum Kind {
+    case wolf
+    case fox
+    case dog
+    case sheep
 }
+
+struct Animal {
+    private var a: Int = 1       //8
+    var b: String = "animal"     //16
+    var c: Kind = .wolf          //8
+    var d: String?      //16
+    var e: Int8 = 8 //8
+}
+
 
 
 public class WGMainVC : UIViewController {
@@ -23,11 +32,11 @@ public class WGMainVC : UIViewController {
     public override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
+        let animal = Animal()
         
-        var stru = DirectionStruct()
-        NSLog("实际占用内存大小:\(MemoryLayout.size(ofValue: stru))\n 系统分配内存大小:\(MemoryLayout.stride(ofValue: stru))\n 内存对齐大小:\(MemoryLayout.alignment(ofValue: stru))")
+        NSLog("实际占用内存大小:\(MemoryLayout.size(ofValue: animal))\n 系统分配内存大小:\(MemoryLayout.stride(ofValue: animal))\n 内存对齐大小:\(MemoryLayout.alignment(ofValue: animal))")
         
-        
+        NSLog("实际占用内存大小:\(MemoryLayout<Int8>.size)\n 系统分配内存大小:\(MemoryLayout.<Int8>.stride)\n 内存对齐大小:\(MemoryLayout.alignment(ofValue: animal))")
     }
 }
 
